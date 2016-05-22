8f8c42b1d90393e1735616cd9db1e3e7266e969f
Pedro <paasp0@gmail.com>
2016-05-22 18:55:42 -0300

    Next (#9)
    * A tela de "Bem vindo" está configurada falta apenas configurar o comando para fechar a aplicação.
    A tela de "Cadastro do Nutricionista" também está completa, só falta o comando de "sair" (fechar) da aplicação.
    A tela principal de menus está configurada com uma idéia básica, não garantindo que continuará assim.
     Criei um activity para o "Cadastro do cliente".
    
     TESTETESTETEST
    
    * Deletei as activity que criei errada. Fiz as alterações e já estão agoracorretas.
    
    * Criei a tela de cadastro de usuário com todos os dados. Foi corrigido um erro no androidManifest para executar o emulador.
    
    * Corrigi alguns erros e terminei algumas configurações de telas
    
    * Adicionei a tela de consultoria e fiz algumas alterações.

--- app/src/main/java/br/com/checknutconcon/checknutcon/BemVindo.java
+++ app/src/main/java/br/com/checknutconcon/checknutcon/BemVindo.java
@@ -1,8 +1,11 @@
 package br.com.checknutconcon.checknutcon;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.*;
 import android.widget.*;
 
@@ -14,24 +17,29 @@ public class BemVindo extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bem_vindo);
+        setContentView(R.layout.bem_vindo);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         btnCadastrar = (Button)findViewById(R.id.btnCadastrar);
         btnSair = (Button)findViewById(R.id.btnSair);
 
         btnCadastrar.setOnClickListener(this);
 
-
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
     }
 
     @Override
     public void onClick(View v) {
 
-
         Intent it = new  Intent (this, CadastroDoNutricionista.class);
         startActivity(it);
-
-
-
     }
 }
---
+++ app/src/main/java/br/com/checknutconcon/checknutcon/ClientePrincipal.java
@@ -0,0 +1,80 @@
+package br.com.checknutconcon.checknutcon;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class ClientePrincipal extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnNovoCliente;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cliente_principal);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btnNovoCliente = (Button)findViewById(R.id.btnNovoCliente);
+        btnNovoCliente.setOnClickListener(this);
+
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_cliente_principal, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.mni_acaoInicial:
+                Intent inicial = new Intent(this, Principal.class);
+                startActivity(inicial);
+
+                break;
+            case R.id.mni_acaoCheckList:
+                Intent checkList = new Intent(this, ConsultoriaPrincipal.class);
+                startActivity(checkList);
+
+                break;
+            case R.id.mni_acaoSair:
+
+                break;
+        }
+
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent it = new Intent(this, NovoCliente.class);
+        startActivity(it);
+
+    }
+}
